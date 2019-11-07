dd17d929c71bf92a83ec914e07548438f1254295
Johanna Pauna <zon.pauna@pp.inet.fi>
2019-11-07 17:23:07 +0200

    Base of start screen

---
+++ Assets/Scripts/StartController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+
+    void Start()
+    {
+        float musicVolume = PlayerPrefs.GetFloat("musicVolume", 1.0f);
+        float soundVolume = PlayerPrefs.GetFloat("soundVolume", 1.0f);
+    }
+
+
+    public void StartButton()
+    {
+        //SceneManager.LoadScene("LevelMenuScene");
+    }
+
+    public void SettingsButton()
+    {
+        SceneManager.LoadScene("SettingScene");
+    }
+
+}
