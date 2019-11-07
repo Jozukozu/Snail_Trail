b2beeb0ab9f002fc7cd1bf79999265624807f4cd
paka00 <t7paka00@students.oamk.fi>
2019-11-07 15:31:32 +0200

    Added refreshrate dropdown and windowed mode toggle

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -1,33 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
-    public Dropdown dropdown;
+    public Dropdown resolutionDropdown;
+    public Dropdown refreshRateDropdown;
+    public Toggle windowedToggle;
     private string[] strlist;
+    private string refRatestr;
+    private int width;
+    private int height;
+    private int refreshRate;
+    private Boolean windowed;
 
-    //void Start()
-    //{
-    //    dropdown = GetComponent<Dropdown>();
-    //    dropdown.onValueChanged.AddListener(delegate
-    //    {
-    //        DropdownValueChanged(dropdown);
-    //    });
-    //}
-
-    public void DropdownValueChanged()
+    void Start()
     {
-        strlist = dropdown.captionText.text.Split('x');
+        width = Screen.width;
+        height = Screen.height;
+        string resolution = width + "x" + height;
+        var listAvailableStrings = resolutionDropdown.options.Select(option => option.text).ToList();
+        resolutionDropdown.value = listAvailableStrings.IndexOf(resolution);
+        Debug.Log(listAvailableStrings.IndexOf(resolution));
+        if (Screen.fullScreen == true)
+        {
+            windowed = false;
+            windowedToggle.isOn = false;
+        } else
+        {
+            windowed = true;
+            windowedToggle.isOn = true;
+        }
+    }
+
+    public void ResolutionDropdownValueChanged()
+    {
+        strlist = resolutionDropdown.captionText.text.Split('x');
+    }
+
+    public void RefreshRateValueChanged()
+    {
+        refRatestr = refreshRateDropdown.captionText.text.Substring(0, 2);
+    }
+
+    public void WindowedToggleChanged()
+    {
+        windowed = windowedToggle.isOn;
     }
 
     public void Apply()
     {
-        int width = int.Parse(strlist[0]);
-        int height = int.Parse(strlist[1]);
-        Debug.Log(strlist[0]);
-        Debug.Log(strlist[1]);
-        Screen.SetResolution(width, height, true);
+        try
+        {
+            width = int.Parse(strlist[0]);
+            height = int.Parse(strlist[1]);
+        } catch(Exception e)
+        {
+            width = 1920;
+            height = 1200;
+        }
+        try
+        {
+            refreshRate = int.Parse(refRatestr);
+        }
+        catch (Exception e)
+        {
+            refreshRate = 0;
+        }
+        Debug.Log(width.ToString() + ", " + height.ToString() + ", " + windowed + ", " + refreshRate.ToString());
+        Screen.SetResolution(width, height, !windowed, refreshRate);
+        Debug.Log(Screen.currentResolution);
     }
 }
