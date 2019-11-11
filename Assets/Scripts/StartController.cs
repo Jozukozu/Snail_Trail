8081cdc873d503cb4ff446524316c312b03a1c5f
paka00 <t7paka00@students.oamk.fi>
2019-11-11 13:01:53 +0200

    Settings work

--- Assets/Scripts/StartController.cs
+++ Assets/Scripts/StartController.cs
@@ -9,13 +9,9 @@ public class StartController : MonoBehaviour
     void Start()
     {
         GameObject.FindGameObjectWithTag("Music").GetComponent<BackgroundMusic>().PlayMusic();
-<<<<<<< HEAD
-
-=======
         float musicVolume = PlayerPrefs.GetFloat("musicVolume", 1.0f);
         float soundVolume = PlayerPrefs.GetFloat("soundVolume", 1.0f);
         GameObject.FindGameObjectWithTag("Music").GetComponent<BackgroundMusic>().SetVolume(musicVolume);
->>>>>>> 75f9ac52da020a638274ddf429509d190d85a84a
     }
 
 
