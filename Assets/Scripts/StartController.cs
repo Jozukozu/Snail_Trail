e379597b77bbc5cb7f1a22b2fb47d966ae0bda9b
Johanna Pauna <zon.pauna@pp.inet.fi>
2019-11-12 13:27:03 +0200

    Button sounds

--- Assets/Scripts/StartController.cs
+++ Assets/Scripts/StartController.cs
@@ -6,23 +6,31 @@ using UnityEngine.SceneManagement;
 public class StartController : MonoBehaviour
 {
 
+    public AudioSource sound;
+
     void Start()
     {
         GameObject.FindGameObjectWithTag("Music").GetComponent<BackgroundMusic>().PlayMusic();
         float musicVolume = PlayerPrefs.GetFloat("musicVolume", 1.0f);
-        float soundVolume = PlayerPrefs.GetFloat("soundVolume", 1.0f);
         GameObject.FindGameObjectWithTag("Music").GetComponent<BackgroundMusic>().SetVolume(musicVolume);
     }
 
 
     public void StartButton()
     {
+        ButtonSound();
         //SceneManager.LoadScene("LevelMenuScene");
     }
 
     public void SettingsButton()
     {
+        ButtonSound();
         SceneManager.LoadScene("SettingScene");
     }
 
+    public void ButtonSound()
+    {
+        sound.volume = PlayerPrefs.GetFloat("soundVolume", 1.0f);
+        sound.Play();
+    }
 }
