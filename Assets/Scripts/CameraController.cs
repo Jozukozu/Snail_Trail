d7382f6985d4d3ec75a34d0ecdea0cfc779b75b8
paka00 <t7paka00@students.oamk.fi>
2019-11-14 17:48:04 +0200

    Snail movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject snail;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - snail.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = snail.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/SnailFlipper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailFlipper : MonoBehaviour
+{
+    public GameObject snail;
+    public GameObject snailHead;
+
+    void FixedUpdate()
+    {
+        //float direction = Input.GetAxis("Horizontal");
+        //if(direction < 0)
+        //{
+        //    snail.transform.localScale = new Vector3(-1, 1, 1);
+        //}
+        //else if(direction > 0)
+        //{
+        //    snail.transform.localScale = new Vector3(1, 1, 1);
+        //}
+    }
+
+    private void LateUpdate()
+    {
+        snail.transform.position = snailHead.transform.position - snailHead.transform.localPosition;
+    }
+}
