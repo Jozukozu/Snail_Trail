13b1978cf0cd80744204da7a46e0a1601a97aa43
Johanna Pauna <zon.pauna@pp.inet.fi>
2019-11-19 13:29:25 +0200

    Snail model moves realistically

---
+++ Assets/Scripts/AnotherSnailController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherSnailController : MonoBehaviour
+{
+
+    public Rigidbody rigidBody;
+    public float speed = 10.0f;
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        rigidBody.AddForce(movement * speed);
+    }
+}
