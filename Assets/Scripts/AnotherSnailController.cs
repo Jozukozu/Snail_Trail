b0610f960c9274bd39c2d2a1eb28977f6b3b688a
Johanna Pauna <zon.pauna@pp.inet.fi>
2019-11-20 15:49:26 +0200

    snail movment sdfgldjh

--- Assets/Scripts/AnotherSnailController.cs
+++ Assets/Scripts/AnotherSnailController.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class AnotherSnailController : MonoBehaviour
 {
 
-
     public float speed;
-    public Rigidbody rigidBody;
-
 
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         Vector3 movement;
-        if (transform.localRotation.x < 90 && transform.localRotation.x > -90)
+        if (transform.localRotation.z < 90 && transform.localRotation.z > -90)
         {
             movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
         }
@@ -36,15 +33,14 @@ public class AnotherSnailController : MonoBehaviour
         {
             Vector3 averageNormal = (leftHitInfo.normal + rightHitInfo.normal) / 2;
             Vector3 averagePoint = (leftHitInfo.point + rightHitInfo.point) / 2;
-            Vector3 correctedPoint = new Vector3(averagePoint.z, averagePoint.y, averagePoint.x);
-            Quaternion targetRotation = Quaternion.FromToRotation(Vector3.right, averageNormal);
+            Quaternion targetRotation = Quaternion.FromToRotation(Vector3.up, averageNormal);
             Quaternion finalRotation = Quaternion.RotateTowards(transform.localRotation, targetRotation, float.PositiveInfinity);
 
 
-            transform.localRotation = Quaternion.Euler(-finalRotation.eulerAngles.z, 90, 90);
+            transform.localRotation = Quaternion.Euler(0, 0, finalRotation.eulerAngles.z);
 
 
-            transform.localPosition = averagePoint + transform.forward * 0.5f;
+            transform.localPosition = averagePoint + transform.up * 0.25f;
         }
     }
 
@@ -52,34 +48,29 @@ public class AnotherSnailController : MonoBehaviour
     private bool GetRaycastDownAtNewPosition(Vector3 movementDirection, out RaycastHit rightHitInfo, out RaycastHit leftHitInfo)
     {
         Vector3 newPosition = transform.localPosition;
-        Vector3 halfy = new Vector3(transform.localScale.y / 2, 0, 0);
+        Vector3 halfX = new Vector3(transform.localScale.x / 2, 0, 0);
         Ray rightRay;
         Ray leftRay;
-
-        Vector3 flip = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
-
-
-
         if (movementDirection.x > 0)
         {
-            rightRay = new Ray(flip + (-transform.right * speed) + transform.localScale.y * transform.right, -transform.forward);
-            leftRay = new Ray(flip + (-transform.right * speed) - transform.localScale.y * transform.right, -transform.forward);
-            Debug.DrawRay(flip + (-transform.right * speed) + transform.localScale.y * transform.right, -transform.forward, Color.green);
-            Debug.DrawRay(flip + (-transform.right * speed) - transform.localScale.y * transform.right, -transform.forward, Color.red);
+            rightRay = new Ray(transform.localPosition + (transform.right * speed) + transform.localScale.x / 2 * transform.right, -transform.up);
+            leftRay = new Ray(transform.localPosition + (transform.right * speed) - transform.localScale.x / 2 * transform.right, -transform.up);
+            Debug.DrawRay(transform.localPosition + (transform.right * speed) + transform.localScale.x / 2 * transform.right, -transform.up, Color.green);
+            Debug.DrawRay(transform.localPosition + (transform.right * speed) - transform.localScale.x / 2 * transform.right, -transform.up, Color.red);
         }
         else if (movementDirection.x < 0)
         {
-            rightRay = new Ray(flip + (transform.right * speed) + transform.localScale.y * transform.right, -transform.forward);
-            leftRay = new Ray(flip + (transform.right * speed) - transform.localScale.y * transform.right, -transform.forward);
-            Debug.DrawRay(flip + (transform.right * speed) + transform.localScale.y * transform.right, -transform.forward, Color.green);
-            Debug.DrawRay(flip + (transform.right * speed) - transform.localScale.y * transform.right, -transform.forward, Color.red);
+            rightRay = new Ray(transform.localPosition + (-transform.right * speed) + transform.localScale.x / 2 * transform.right, -transform.up);
+            leftRay = new Ray(transform.localPosition + (-transform.right * speed) - transform.localScale.x / 2 * transform.right, -transform.up);
+            Debug.DrawRay(transform.localPosition + (-transform.right * speed) + transform.localScale.x / 2 * transform.right, -transform.up, Color.green);
+            Debug.DrawRay(transform.localPosition + (-transform.right * speed) - transform.localScale.x / 2 * transform.right, -transform.up, Color.red);
         }
         else
         {
-            rightRay = new Ray(flip + transform.localScale.y * transform.right, -transform.forward);
-            leftRay = new Ray(flip - transform.localScale.y * transform.right, -transform.forward);
-            Debug.DrawRay(flip + transform.localScale.y * transform.right, -transform.forward, Color.green);
-            Debug.DrawRay(flip - transform.localScale.y * transform.right, -transform.forward, Color.red);
+            rightRay = new Ray(transform.localPosition + transform.localScale.x / 2 * transform.right, -transform.up);
+            leftRay = new Ray(transform.localPosition - transform.localScale.x / 2 * transform.right, -transform.up);
+            Debug.DrawRay(transform.localPosition + transform.localScale.x / 2 * transform.right, -transform.up, Color.green);
+            Debug.DrawRay(transform.localPosition - transform.localScale.x / 2 * transform.right, -transform.up, Color.red);
         }
         bool rightCast = Physics.Raycast(rightRay, out rightHitInfo, 2, LayerMask.GetMask("Ground"));
         bool leftCast = Physics.Raycast(leftRay, out leftHitInfo, 2, LayerMask.GetMask("Ground"));
@@ -93,15 +84,5 @@ public class AnotherSnailController : MonoBehaviour
 
         return false;
     }
-
-
-
-
-
-
-
-
-
-
 
 }
--- Assets/Scripts/SnailTailController.cs
+++ Assets/Scripts/SnailTailController.cs
@@ -4,20 +4,86 @@ using UnityEngine;
 
 public class SnailTailController : MonoBehaviour
 {
-    public Rigidbody rigidBody;
-    public float stick;
+    public float speed;
+    public GameObject root;
 
 
-    void FixedUpdate()
+
+    void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-
-        if (moveHorizontal != 0)
+        Vector3 movement;
+        if (transform.localRotation.z < 90 && transform.localRotation.z > -90)
         {
-            Vector3 downForce = new Vector3(0.0f, -1.0f, 0.0f);
-            rigidBody.AddForce(downForce * stick);
+            movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        }
+        else
+        {
+            movement = new Vector3(-moveHorizontal, 0.0f, 0.0f);
+        }
+        UpdatePlayerTransform(movement);
+    }
+
+
+    private void UpdatePlayerTransform(Vector3 movementDirection)
+    {
+        RaycastHit rightHitInfo;
+
+
+
+        if (GetRaycastDownAtNewPosition(movementDirection, out rightHitInfo))
+        {
+            Vector3 averageNormal = rightHitInfo.normal;
+            Vector3 averagePoint = rightHitInfo.point;
+            Vector3 correctedPoint = new Vector3(averagePoint.z, averagePoint.y, averagePoint.x);
+            Quaternion targetRotation = Quaternion.FromToRotation(Vector3.forward, averageNormal);
+            Quaternion finalRotation = Quaternion.RotateTowards(transform.localRotation, targetRotation, float.PositiveInfinity);
+
+
+            transform.localRotation = Quaternion.Euler(finalRotation.eulerAngles.z, 0, 0);
+
+
+            transform.localPosition = correctedPoint + transform.forward * 0.25f;
+        }
+    }
+
+
+    private bool GetRaycastDownAtNewPosition(Vector3 movementDirection, out RaycastHit rightHitInfo)
+    {
+        Vector3 newPosition = transform.localPosition;
+        Vector3 halfX = new Vector3(transform.localScale.x / 2, 0, 0);
+
+        Vector3 offset = new Vector3(0, 0, 0);
+        Vector3 rootAxis = new Vector3(root.transform.localPosition.x, root.transform.localPosition.y, root.transform.localPosition.z);
+        Ray rightRay;
+
+        if (movementDirection.x > 0)
+        {
+            rightRay = new Ray(rootAxis - offset + root.transform.right * speed, -root.transform.up);
+            Debug.DrawRay(rootAxis - offset + root.transform.right * speed, -root.transform.up, Color.green);
+
+        }
+        else if (movementDirection.x < 0)
+        {
+            rightRay = new Ray(rootAxis - offset - root.transform.right * speed, -root.transform.up);
+            Debug.DrawRay(rootAxis - offset - root.transform.right * speed, -root.transform.up, Color.green);
+        }
+        else
+        {
+            rightRay = new Ray(rootAxis - offset, -root.transform.up);
+            Debug.DrawRay(rootAxis - offset, -root.transform.up, Color.green);
+        }
+        bool rightCast = Physics.Raycast(rightRay, out rightHitInfo, 2, LayerMask.GetMask("Ground"));
+
+
+
+        if (rightCast)
+        {
+            return true;
         }
 
+
+        return false;
     }
 
 }
