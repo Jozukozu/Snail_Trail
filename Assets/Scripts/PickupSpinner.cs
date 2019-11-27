06c8d34de5b4fafed06e5500501ae231f90b4bb3
paka00 <t7paka00@students.oamk.fi>
2019-11-27 16:49:30 +0200

    Strawberry model and material

---
+++ Assets/Scripts/PickupSpinner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpinner : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 40, 0) * Time.deltaTime, Space.World);
+    }
+}
