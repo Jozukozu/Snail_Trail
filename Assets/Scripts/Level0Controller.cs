176cdab9cf3935bb5e2844bcced97fa7ea4a6032
Johanna Pauna <zon.pauna@pp.inet.fi>
2019-12-03 12:29:39 +0200

    Clean up

---
+++ Assets/Scripts/Level0Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level0Controller : MonoBehaviour
+{
+
+    public static int baseScore;
+    public static int energyScore;
+
+    void Start()
+    {
+        baseScore = 0;
+        energyScore = 0;
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+
+
+
+
+
+    }
+}
